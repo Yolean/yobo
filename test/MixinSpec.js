a774141737dee76bfd20e18079231dd4bd34d6a4
Staffan Olsson <staffan@repos.se>
2015-03-22 13:24:57 +0100

    specs

---
+++ test/MixinSpec.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+describe("#mixin", function() {
+
+  var yobo = require('../');
+
+  it("Is a function on Model and Collection", function() {
+    expect(yobo.Model.mixin).to.be.a('function');
+    expect(yobo.Collection.mixin).to.be.a('function');
+  });
+
+  xit("Is a function on View, Router, History", function() {
+
+  });
+
+  it("Is not allowed directly on those, so do a dummy '.extend({})' unless you already specialized", function() {
+    expect(function() {
+      var MyModel = yobo.Model.mixin({});
+    }).to.throw('Forbidden to modify exported Model, use .extend({}) first');
+  });
+
+  describe("Given a single object", function() {
+
+    it("Behaves like .extend if the props don't exist on super", function() {
+      var MyModel = yobo.Model.extend({}).mixin({
+        myProp: true
+      });
+      expect(MyModel.prototype.myProp).to.be.true();
+    });
+
+    it("Composes behavior on prop collision", function() {
+      var happened = [];
+      var MySpecial = yobo.Model.extend({
+        myFn: function() {
+          happened.push('A');
+        }
+      });
+      MySpecial.mixin({
+        myFn: function() {
+          happened.push('B');
+        }
+      });
+      expect(happened).to.deep.equal(['A','B']);
+    });
+
+    xit("Preserves order of composition to order of execution", function() {
+
+    });
+
+    xit("Runs just as well on instances", function() {
+
+    });
+
+  });
+
+  describe("Given string and function", function() {
+
+    it("Is the same as a mixin object with only one prop", function() {
+      var fn = function() {};
+      var MyModel = yobo.Model.extend({}).mixin('myProp', fn);
+      expect(MyModel.prototype.myProp).to.equal(fn);
+    });
+
+  });
+
+  xit("What if we want to modify existing behavior without extending? Would AOP style wrapping be useful?");
+
+});
