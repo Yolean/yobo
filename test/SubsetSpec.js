179ab8222a1c799538a0c051daeec77785fa33b7
Staffan Olsson <staffan@repos.se>
2015-04-09 10:01:54 +0200

    Coverage for subset-subset immerse

--- test/SubsetSpec.js
+++ test/SubsetSpec.js
@@ -45,6 +45,18 @@ describe('Subset', function() {
       expect(subset2.length).to.equal(1);
     });
 
+    it("Aggregates immerse for each layer of subset", function() {
+      var superset = new Collection();
+      var subset1 = superset.subset(function (m) { return true; }, function(m) { m.set('1', true); });
+      var subset2 = subset1.subset(function (m) { return true; }, function(m) { m.set('2', true); });
+      var add1 = subset1.add(new Model({}));
+      var add2 = subset2.add(new Model({}));
+      expect(add1.has('1')).to.be.true;
+      expect(add1.has('2')).to.be.false;
+      expect(add2.has('1')).to.be.true;
+      expect(add2.has('2')).to.be.true;
+    });
+
   });
 
   describe("#subsetWhere", function() {
